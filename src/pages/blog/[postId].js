e6700a659e0a3a546aef07edb8ef9420c3226236
SmeraldaKa0s <anabella.lerner@gmail.com>
2022-05-10 19:03:03 -0300

    added metatags

--- src/pages/blog/[postId].js
+++ src/pages/blog/[postId].js
@@ -16,6 +16,7 @@ const Post = ({ data, content }) => {
   const {
     title = "Default Title",
     img = '/post_images/default.png',
+    description = "",
   } = data;
 
   return (
@@ -23,9 +24,23 @@ const Post = ({ data, content }) => {
       <Navbar color="black" />
       <Head>
         <title>{title}</title>
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content="https://estudiosande.com" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={img} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://estudiosande.com" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={img} />
+
       </Head>
       <div className='py-12 px-8 text-justify flex flex-col items-center justify-center m-auto gap-5'>
-        <h1 className='text-5xl text-bold text-red-800 mb-10'>{title}</h1>
         <div className="max-w-5xl w-full">
           <Image src={img} layout="responsive" width="800" height="350" />
         </div>
